05f8bb3c4ea32102df417a52745f4c6c91c20067
Andi Demi <107476532+AndiTheDef@users.noreply.github.com>
2022-08-30 00:03:16 +0200

    Update ShowCategorySelected.js

--- src/components/molecules/ShowCategorySelected.js
+++ src/components/molecules/ShowCategorySelected.js
@@ -28,29 +28,32 @@ const ShowCategorySelected = ({ name, searchInput }) => {
                 return val;
               }
             })
-            .map((item) => (
-              <p key={item.id}>
-                {item.text}
-                <span onClick={() => removeTodo(item.id, foodParser, "food")}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-x-circle"
-                  >
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="15" y1="9" x2="9" y2="15"></line>
-                    <line x1="9" y1="9" x2="15" y2="15"></line>
-                  </svg>
-                </span>
-              </p>
-            ))
+            .map((item) => {
+              if (Object.keys(item).length === 0) return;
+              return (
+                <p key={item.id}>
+                  {item.text}
+                  <span onClick={() => removeTodo(item.id, foodParser, "food")}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-x-circle"
+                    >
+                      <circle cx="12" cy="12" r="10"></circle>
+                      <line x1="15" y1="9" x2="9" y2="15"></line>
+                      <line x1="9" y1="9" x2="15" y2="15"></line>
+                    </svg>
+                  </span>
+                </p>
+              );
+            })
         : name === "fitnes"
         ? (fitnesParser || [])
             .filter((val) => {
@@ -62,31 +65,34 @@ const ShowCategorySelected = ({ name, searchInput }) => {
                 return val;
               }
             })
-            .map((item) => (
-              <p key={item.id}>
-                {item.text}
-                <span
-                  onClick={() => removeTodo(item.id, fitnesParser, "fitnes")}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-x-circle"
+            .map((item) => {
+              if (Object.keys(item).length === 0) return;
+              return (
+                <p key={item.id}>
+                  {item.text}
+                  <span
+                    onClick={() => removeTodo(item.id, fitnesParser, "fitnes")}
                   >
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="15" y1="9" x2="9" y2="15"></line>
-                    <line x1="9" y1="9" x2="15" y2="15"></line>
-                  </svg>
-                </span>
-              </p>
-            ))
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-x-circle"
+                    >
+                      <circle cx="12" cy="12" r="10"></circle>
+                      <line x1="15" y1="9" x2="9" y2="15"></line>
+                      <line x1="9" y1="9" x2="15" y2="15"></line>
+                    </svg>
+                  </span>
+                </p>
+              );
+            })
         : name === "work"
         ? (workParser || [])
             .filter((val) => {
@@ -98,29 +104,33 @@ const ShowCategorySelected = ({ name, searchInput }) => {
                 return val;
               }
             })
-            .map((item) => (
-              <p key={item.id}>
-                {item.text}
-                <span onClick={() => removeTodo(item.id, workParser, "work")}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-x-circle"
-                  >
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="15" y1="9" x2="9" y2="15"></line>
-                    <line x1="9" y1="9" x2="15" y2="15"></line>
-                  </svg>
-                </span>
-              </p>
-            ))
+            .map((item) => {
+              if (Object.keys(item).length === 0) return;
+
+              return (
+                <p key={item.id}>
+                  {item.text}
+                  <span onClick={() => removeTodo(item.id, workParser, "work")}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-x-circle"
+                    >
+                      <circle cx="12" cy="12" r="10"></circle>
+                      <line x1="15" y1="9" x2="9" y2="15"></line>
+                      <line x1="9" y1="9" x2="15" y2="15"></line>
+                    </svg>
+                  </span>
+                </p>
+              );
+            })
         : name === "learn"
         ? (learnParser || [])
             .filter((val) => {
@@ -132,29 +142,33 @@ const ShowCategorySelected = ({ name, searchInput }) => {
                 return val;
               }
             })
-            .map((item) => (
-              <p key={item.id}>
-                {item.text}
-                <span onClick={() => removeTodo(item.id, learnParser, "learn")}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-x-circle"
+            .map((item) => {
+              return (
+                <p key={item.id}>
+                  {item.text}
+                  <span
+                    onClick={() => removeTodo(item.id, learnParser, "learn")}
                   >
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="15" y1="9" x2="9" y2="15"></line>
-                    <line x1="9" y1="9" x2="15" y2="15"></line>
-                  </svg>
-                </span>
-              </p>
-            ))
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-x-circle"
+                    >
+                      <circle cx="12" cy="12" r="10"></circle>
+                      <line x1="15" y1="9" x2="9" y2="15"></line>
+                      <line x1="9" y1="9" x2="15" y2="15"></line>
+                    </svg>
+                  </span>
+                </p>
+              );
+            })
         : ""}
     </div>
   );
